7ceffe729a98329b50c0ae600c30ce4030f8f0aa
Pan Luo <pan.luo@ubc.ca>
2014-12-10 16:39:18 -0800

    Add autoload to bootstrap.php

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,4 +1,6 @@
 <?php
+// standard composer install vendor autoload magic
+require dirname(__DIR__) . '/vendor/autoload.php';
 
 // Bootstrap the JMS custom annotations for Object to XML/Json mapping
 \Doctrine\Common\Annotations\AnnotationRegistry::registerAutoloadNamespace(
